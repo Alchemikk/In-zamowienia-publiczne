8dfd2b9ec4f6afde31909b0e6fcfd6df530164b0
Kajetan Kaczmarek <Kajetan Kaczmarek@LAPTOP-2OLM88US>
2018-08-21 16:20:31 +0200

    initial

---
+++ Test Suites/Logowanie.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Wejscie na strone + logowanie poprawne</description>
+   <name>Logowanie</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-21T14:56:24</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>279e8f9e-397a-4feb-abba-e06d6066bcb5</testSuiteGuid>
+   <testCaseLink>
+      <guid>7ac6e20f-8bdf-4f7c-b473-196097cf6b47</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logowanie/Wejscie na strone</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bf3b1859-d5e0-4978-8c91-22e1f764854a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logowanie/Logowanie poprawne</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
